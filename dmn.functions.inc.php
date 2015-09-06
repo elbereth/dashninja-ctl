3738d32d0788eb69dfb48e2050f05af1601c4a2a
Alexandre Devilliers <elbereth@dashninja.pl>
2015-09-07 00:50:09 +0200

    Added main control script

--- dmn.functions.inc.php
+++ dmn.functions.inc.php
@@ -221,4 +221,16 @@ function getipport($addr) {
   return array($ip,$port);
 }
 
+// Random password generator
+function randomPassword($length = 8) {
+  $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+  $pass = array(); //remember to declare $pass as an array
+  $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+  for ($i = 0; $i < $length; $i++) {
+    $n = rand(0, $alphaLength);
+    $pass[] = $alphabet[$n];
+  }
+  return implode($pass); //turn the array into a string
+}
+
 ?>
